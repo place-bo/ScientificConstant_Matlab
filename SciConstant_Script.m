9405a7b798358f253f198eb2b9fee7a3bc182f6c
QXY <1424683706@qq.com>
2025-01-20 07:54:37 +0800

    This is the first edition for SciConstant_Matlab

---
+++ SciConstant_Script.m
@@ -0,0 +1,36 @@
+clc,clear
+keys     = {'q',...   
+            'k_B',...  
+            'k_Be',...
+            'c',...
+            'epsilon_0',...
+            'm_0',...
+            'eV',...
+            'h',...
+            'h_bar',...
+            'N_A',...
+            'T_0',...
+            'm_0'
+            };    
+value_unit    = {struct('name','elementary charge','value', 1.602176634*10^-19,'unit','C'),
+                 struct('name','Boltzmann constant','value', 1.380649   *10^-23,'unit','J/K'),
+                 struct('name','Boltzmann constant(Electronic)','value',8.617*10^-5,'unit','eV/K'),
+                 struct('name','speed of light','value',2.998E8,'unit','m/s'),
+                 struct('name','dielectric constant of vacuum','value',8.8541878188*10^-12,'unit','F/m'),
+                 struct('name','electron rest mass','value',9.108e-31,'unit','kg'),
+                 struct('name','electron volt','value', 1.602176634E-19,'unit','J'),
+                 struct('name','Plank Constant','value',6.625E-34,'unit','J·s'),
+                 struct('name','reduced Planck constant','value',1.054e-34,'unit','J·s'),
+                 struct('name','Avogadro constant','value',6.025E23,'unit','mol-1'),
+                 struct('name','absolute zero','value',273.16,'unit','K')
+                 struct('name','free electron mass','value',9.10938356E-31,'unit','kg')
+                 };
+SciConstant = containers.Map(keys,value_unit);
+clearvars -except SciConstant
+save("SciConstant.mat")
+%% usage 
+clc,clear
+load("SciConstant.mat");
+SciConstant('c').value
+SciConstant('c').unit
+SciConstant('c').name
